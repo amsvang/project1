ea4d74b855a605f09a17995427decd4e8038d34d
HelloNikoG <nikog.nyc@gmail.com>
2022-02-09 08:57:40 -0800

    reimbursement dao

--- src/main/java/com/revature/daos/ReimbursementDAO.java
+++ src/main/java/com/revature/daos/ReimbursementDAO.java
@@ -1,14 +1,25 @@
 package com.revature.daos;
 
-import com.revature.model.Reimbursement;
-import com.revature.model.ReimbursementStatus;
-import com.revature.model.ReimbursementType;
+import com.revature.model.*;
+
+import java.util.List;
 
 public interface ReimbursementDAO {
 
+//    public boolean addReimbursement(Reimbursement reimbursement);
+//    public boolean updateReimbursement();
+//    public boolean deleteReimbursement();
+//    public ReimbursementStatus status(String status);
+//    public ReimbursementType type(String type);
+
     public boolean addReimbursement(Reimbursement reimbursement);
-    public boolean updateReimbursement();
+    public boolean updateReimbursement(Reimbursement reimbursement);
     public boolean deleteReimbursement();
-    public ReimbursementStatus status(String status);
-    public ReimbursementType type(String type);
+    public List<Reimbursement> getAllReimbursements();
+    public List<Reimbursement> getAllReimbursementsByStatus(int id);
+    public List<Reimbursement> getAllReimbursementsByUsernameAndStatus(String username, int id);
+    public Reimbursement getReimbursementById(int id);
+
+
 }
+
